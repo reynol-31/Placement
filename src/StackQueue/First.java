ddd3aec50bdecefe56c70bad4a8c4abb77aba5da
reynol-31 <reynol.23cs119@sode-edu.in>
2025-12-02 12:13:13 +0530

    add

--- src/StackQueue/First.java
+++ src/StackQueue/First.java
@@ -5,7 +5,9 @@ class Main{
         o.push(10);
         o.push(20);
         o.push(30);
+        o.pop();
         o.dis();
+        o.peek();
     }
 }
 class Stack {
@@ -22,8 +24,23 @@ class Stack {
             arr[++top]=data;
     }
     public void dis(){
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
+        if(top==-1)
+            System.out.println("Empty Stack");
+        else
+            for(int i=0;i<arr.length;i++){
+                System.out.println(arr[i]+" ");
+            }
+    }
+    public void peek(){
+        if(top==-1)
+            System.out.println("Empty Stack");
+        else
+            System.out.println(arr[top]);
+    }
+    public void pop(){
+        if(top==-1)
+            System.out.println("Empty Stack");
+        else
+            top--;
     }
 }
